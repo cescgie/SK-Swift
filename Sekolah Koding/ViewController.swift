bd660cb13f7b85e34dde3c2ddf2495a139fa39b3
Yoggi Firmanda <y.firmanda@netpoint-media.de>
2016-02-24 13:50:13 +0100

    Ios App

--- Sekolah Koding/ViewController.swift
+++ Sekolah Koding/ViewController.swift
@@ -9,17 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let urls = "https://www.sekolahkoding.com"
+        let url = NSURL (string: urls)
+        let requestObj = NSURLRequest(URL: url!)
+        webView?.loadRequest(requestObj);
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
